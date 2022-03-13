b8448135f41e9ef081cc18880530d60666ce3c5c
patrickmx <97127469+patrickmx@users.noreply.github.com>
2022-03-13 16:57:58 +0100

    adds mailhog config options in the api

--- api/v1alpha1/mailhoginstance_types.go
+++ api/v1alpha1/mailhoginstance_types.go
@@ -16,26 +16,109 @@ limitations under the License.
 
 package v1alpha1
 
+// Important: Run "make" to regenerate code after modifying this file
+
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // MailhogInstanceSpec defines the desired state of MailhogInstance
 type MailhogInstanceSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of MailhogInstance. Edit mailhoginstance_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Image is the mailhog image to be used
+	//
+	//+kubebuilder:validation:Required
+	//+kubebuilder:validation:MinLength=4
+	//+kubebuilder:default:="mailhog/mailhog:latest"
+	Image string `json:"image,omitempty"`
+
+	// Replicas is the count of pods to create
+	//
+	//+kubebuilder:validation:Minimum=0
+	//+kubebuilder:validation:Maximum=10
+	//+kubebuilder:validation:Required
+	//+kubebuilder:default:=1
+	Replicas int32 `json:"replicas,omitempty"`
+
+	// Settings are mailhog configuration options, see https://github.com/mailhog/MailHog/blob/master/docs/CONFIG.md
+	//
+	//+kubebuilder:validation:Optional
+	//+kubebuilder:default:={storage:"memory"}
+	Settings MailhogInstanceSettingsSpec `json:"settings,omitempty"`
+
+	// WebTrafficInlet defines how the webinterface is exposed
+	//
+	//+kubebuilder:validation:Required
+	//+kubebuilder:default:="none"
+	//+kubebuilder:validation:Enum=none;route
+	WebTrafficInlet string `json:"webTrafficInlet,omitempty"`
+}
+
+// MailhogInstanceSettingsSpec are settings related to the mailhog instance
+type MailhogInstanceSettingsSpec struct {
+
+	// Hostname is the hostname for smtp ehlo/helo
+	//
+	//+kubebuilder:validation:Optional
+	Hostname string `json:"hostname,omitempty"`
+
+	// CorsOrigin if set, this value is added into the Access-Control-Allow-Origin header returned by the API
+	//
+	//+kubebuilder:validation:Optional
+	CorsOrigin string `json:"corsOrigin,omitempty"`
+
+	// Storage which storage backend to use, eg memory
+	//
+	//+kubebuilder:validation:Enum=memory;maildir;mongodb
+	//+kubebuilder:validation:Optional
+	//+kubebuilder:default:="memory"
+	Storage string `json:"storage,omitempty"`
+
+	// StorageMongoDb are only used when storage is set to mongodb
+	//
+	//+kubebuilder:validation:Optional
+	StorageMongoDb MailhogStorageMongoDbSpec `json:"storageMongoDb,omitempty"`
+
+	// StorageMaildir is only used when storage is set to maildir
+	//
+	//+kubebuilder:validation:Optional
+	StorageMaildir MailhogStorageMaildirSpec `json:"storageMaildir,omitempty"`
+}
+
+// MailhogStorageMaildirSpec are settings applicable if the storage backend is maildir
+type MailhogStorageMaildirSpec struct {
+
+	// Path Maildir path (for maildir storage backend)
+	//
+	//+kubebuilder:validation:Optional
+	Path string `json:"path,omitempty"`
+}
+
+// MailhogStorageMongoDbSpec are settings applicable if the storage backend is mongodb
+type MailhogStorageMongoDbSpec struct {
+
+	// Uri MongoDB host and port
+	//
+	//+kubebuilder:validation:Optional
+	Uri string `json:"uri,omitempty"`
+
+	// Db MongoDB database name for message storage
+	//
+	//+kubebuilder:validation:Optional
+	Db string `json:"db,omitempty"`
+
+	// Collection MongoDB collection name for message storage
+	//
+	//+kubebuilder:validation:Optional
+	Collection string `json:"collection,omitempty"`
 }
 
 // MailhogInstanceStatus defines the observed state of MailhogInstance
 type MailhogInstanceStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+
+	// Pods all the podnames owned by the cr
+	//
+	Pods []string `json:"pods,omitempty"`
 }
 
 //+kubebuilder:object:root=true
