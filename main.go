1d17bfce9b2f02dfded653f735d4e9139da4ff55
patrickmx <97127469+patrickmx@users.noreply.github.com>
2022-03-13 17:06:22 +0100

    adds route to client scheme

--- main.go
+++ main.go
@@ -31,6 +31,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
+	routev1 "github.com/openshift/api/route/v1"
+
 	mailhogv1alpha1 "goimports.patrick.mx/mailhog-operator/api/v1alpha1"
 	"goimports.patrick.mx/mailhog-operator/controllers"
 	//+kubebuilder:scaffold:imports
@@ -43,6 +45,8 @@ var (
 
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+
+	utilruntime.Must(routev1.AddToScheme(scheme))
 
 	utilruntime.Must(mailhogv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
