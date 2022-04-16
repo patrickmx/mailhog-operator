c4903abc8e867e8f85d950c4efc2a22fb6cda64d
patrickmx <97127469+patrickmx@users.noreply.github.com>
2022-04-16 14:49:05 +0200

    refactor status to make it easier to extend

--- controllers/status.go
+++ controllers/status.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"reflect"
 
+	"github.com/go-logr/logr"
+
 	mailhogv1alpha1 "goimports.patrick.mx/mailhog-operator/api/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -12,25 +14,15 @@ import (
 
 // ensureStatus reconciles the status subresource of the MailhogInstance CR
 func (r *MailhogInstanceReconciler) ensureStatus(ctx context.Context, cr *mailhogv1alpha1.MailhogInstance) *ReturnIndicator {
-	var err error
 	name := types.NamespacedName{Name: cr.Name, Namespace: cr.Namespace}
-	meta := CreateMetaMaker(cr)
 	logger := r.logger.WithValues(span, spanCrStatus)
 
-	podList := &corev1.PodList{}
-	listOpts := []client.ListOption{
-		client.InNamespace(cr.Namespace),
-		client.MatchingLabels(meta.GetLabels(false)),
+	ri, desiredStatus := r.desiredStatus(ctx, cr, logger)
+	if ri != nil {
+		return ri
 	}
-	if err = r.List(ctx, podList, listOpts...); err != nil {
-		logger.Error(err, failedListPods)
-		return &ReturnIndicator{
-			Err: err,
-		}
-	}
-	podNames := getPodNames(podList.Items)
 
-	if !reflect.DeepEqual(podNames, cr.Status.Pods) {
+	if !reflect.DeepEqual(desiredStatus, cr.Status) {
 		update := &mailhogv1alpha1.MailhogInstance{}
 		if err := r.Get(ctx, name, update); err != nil {
 			logger.Error(err, failedCrRefresh)
@@ -38,9 +30,7 @@ func (r *MailhogInstanceReconciler) ensureStatus(ctx context.Context, cr *mailho
 				Err: err,
 			}
 		}
-		update.Status.Pods = podNames
-		update.Status.PodCount = len(podNames)
-		update.Status.LabelSelector = meta.GetSelector(true)
+		update.Status = desiredStatus
 		if err := r.Status().Update(ctx, update); err != nil {
 			logger.Error(err, failedCrUpdateStatus)
 			return &ReturnIndicator{
@@ -50,7 +40,6 @@ func (r *MailhogInstanceReconciler) ensureStatus(ctx context.Context, cr *mailho
 		logger.Info(updatedCrStatus)
 		crUpdate.Inc()
 		return &ReturnIndicator{}
-
 	}
 
 	logger.Info(noCrUpdateNeeded)
@@ -64,4 +53,27 @@ func getPodNames(pods []corev1.Pod) []string {
 		podNames = append(podNames, pod.Name)
 	}
 	return podNames
+}
+
+func (r *MailhogInstanceReconciler) desiredStatus(ctx context.Context, cr *mailhogv1alpha1.MailhogInstance, logger logr.Logger) (ri *ReturnIndicator, status mailhogv1alpha1.MailhogInstanceStatus) {
+	meta := CreateMetaMaker(cr)
+
+	podList := &corev1.PodList{}
+	listOpts := []client.ListOption{
+		client.InNamespace(cr.Namespace),
+		client.MatchingLabels(meta.GetLabels(false)),
+	}
+	if err := r.List(ctx, podList, listOpts...); err != nil {
+		logger.Error(err, failedListPods)
+		return &ReturnIndicator{
+			Err: err,
+		}, status
+	}
+
+	podNames := getPodNames(podList.Items)
+	status.Pods = podNames
+	status.PodCount = len(podNames)
+	status.LabelSelector = meta.GetSelector(true)
+	status.Error = ""
+	return nil, status
 }
